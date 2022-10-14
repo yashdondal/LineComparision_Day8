44a6b1d2fa4c3bbf4ab9715e8c02411310fddee6
yashdondal <yashdondal21@gmail.com>
2022-10-14 20:08:29 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LineComparsion_DAY8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Console.WriteLine("Welcome to line comparision problem");
+
+            //CalculateLength calculateLength = new CalculateLength();
+            //calculateLength.CalculateLengthofLine();
+
+            //CheckEquality checkEquality = new CheckEquality();  
+            //checkEquality.CalculateLengthofLine();
+
+            LineEqual lineequal = new LineEqual();
+            lineequal.ToCompareTwoLines();
+
+
+
+
+        }
+    }
+}
