55fb67d6833ac75cccb43ef2eed88f7ceaa34ebc
yashdondal <yashdondal21@gmail.com>
2022-10-14 20:10:37 +0530

    calulate the length

---
+++ CalculateLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparsion_DAY8
+{
+    public class CalculateLength
+    {
+        public void CalculateLengthofLine()
+        {
+            int x1 = 12;
+            int x2 = 3;
+            int y1 = 5;
+            int y2 = 9;
+
+
+            double LengOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("\n Length of Line is : {0}", LengOfLine);
+
+        }
+    }
+}
+
+ 
